fb74d0e17b06895a7fb276c3366bb9ae86ab7104
David Murad <nizzar.halabi@googlemail.com>
2022-04-13 01:20:50 +0200

    responsive cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -7,16 +7,17 @@ const Cart = (props) => {
     <div className="cart-active">
       <div className="cart-header">
         <p>Cart</p>
-        <button onClick={() => toggleClose()}>Close</button>
       </div>
       <p className="items">
         {orders?.length > 0
           ? orders.map((x, key) => <p key={key}>{x}</p>)
           : "Currently no orders"}
       </p>
+      <p>Total: {total}</p>
+
       <div className="cart-header">
-        <p>Total: {total}</p>
         <button onClick={() => confirmOrder()}>finish order</button>
+        <button onClick={() => toggleClose()}>Close</button>
       </div>
     </div>
   );
